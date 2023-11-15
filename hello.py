c93529ac77395ab9a2072ae33d5a5edac55308a7
aidenbarkerVT <aidenbarker@vt.edu>
2023-11-15 14:04:30 -0500

    combined hello.py

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("Hello, my name is Aiden Barker") # aidens code
+print("Hello, my name is Eunice") # eunices code
+print("Hello, my name is Morgan") # morgans code
+print("Hello, my name is Vatsa Bhatt") # vatsas code
